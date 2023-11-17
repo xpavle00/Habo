0ca3f27f912ffc60395e5c7da245dac7b05fe416
Peter Pavlenko <pavlenko.p@gmail.com>
2023-11-17 18:21:22 +0100

    Added generated languages

---
+++ lib/generated/intl/messages_cs.dart
@@ -0,0 +1,181 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a cs locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'cs';
+
+  static String m0(theme) => "${Intl.select(theme, {
+            'device': 'Zařízení',
+            'light': 'Světlá',
+            'dark': 'Tmavá',
+            'oled': 'OLED černá',
+            'other': 'Zařízení',
+          })}";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "about": MessageLookupByLibrary.simpleMessage("O aplikaci"),
+        "accountabilityPartner":
+            MessageLookupByLibrary.simpleMessage("Smluvni partner"),
+        "add": MessageLookupByLibrary.simpleMessage("Přidat"),
+        "advancedHabitBuilding":
+            MessageLookupByLibrary.simpleMessage("Pokročilé budování návyků"),
+        "advancedHabitBuildingDescription": MessageLookupByLibrary.simpleMessage(
+            "Tato část vám pomáhá lépe definovat vaše návyky s využitím Smyčky návyků. Měli byste definovat podněty, rutiny a odměny pro každý návyk."),
+        "allHabitsWillBeReplaced": MessageLookupByLibrary.simpleMessage(
+            "Všechny návyky budou nahrazeny návyky ze zálohy."),
+        "allow": MessageLookupByLibrary.simpleMessage("Povolit"),
+        "appNotifications":
+            MessageLookupByLibrary.simpleMessage("Oznámení aplikace"),
+        "appNotificationsChannel":
+            MessageLookupByLibrary.simpleMessage("Kanál oznámení pro aplikaci"),
+        "at7AM": MessageLookupByLibrary.simpleMessage("V 7:00"),
+        "backup": MessageLookupByLibrary.simpleMessage("Záloha"),
+        "backupFailedError": MessageLookupByLibrary.simpleMessage(
+            "CHYBA: Vytvoření zálohy se nezdařilo."),
+        "buyMeACoffee": MessageLookupByLibrary.simpleMessage("Koupit mi kávu"),
+        "cancel": MessageLookupByLibrary.simpleMessage("Zrušit"),
+        "check": MessageLookupByLibrary.simpleMessage("Zkontrolovat"),
+        "close": MessageLookupByLibrary.simpleMessage("Zavřít"),
+        "congratulationsReward":
+            MessageLookupByLibrary.simpleMessage("Gratulujeme! Vaše odměna:"),
+        "copyright": MessageLookupByLibrary.simpleMessage("©2023 Habo"),
+        "create": MessageLookupByLibrary.simpleMessage("Vytvořit"),
+        "createHabit": MessageLookupByLibrary.simpleMessage("Vytvořit návyk"),
+        "createYourFirstHabit":
+            MessageLookupByLibrary.simpleMessage("Vytvořte svůj první návyk."),
+        "cue": MessageLookupByLibrary.simpleMessage("Podnět"),
+        "cueDescription": MessageLookupByLibrary.simpleMessage(
+            "je spouštěč, který iniciuje váš návyk. Může to být konkrétní čas, místo, pocit nebo událost."),
+        "cueNumbered": MessageLookupByLibrary.simpleMessage("1. Podnět"),
+        "currentStreak": MessageLookupByLibrary.simpleMessage("Současná série"),
+        "dan": MessageLookupByLibrary.simpleMessage("Dan"),
+        "date": MessageLookupByLibrary.simpleMessage("Datum"),
+        "defineYourHabits":
+            MessageLookupByLibrary.simpleMessage("Definujte své návyky"),
+        "defineYourHabitsDescription": MessageLookupByLibrary.simpleMessage(
+            "Aby se vám lépe dařilo dodržovat své návyky, můžete definovat:"),
+        "delete": MessageLookupByLibrary.simpleMessage("Smazat"),
+        "disclaimer": MessageLookupByLibrary.simpleMessage("Prohlášení"),
+        "do50PushUps": MessageLookupByLibrary.simpleMessage("Udělat 50 kliků"),
+        "doNotForgetToCheckYourHabits": MessageLookupByLibrary.simpleMessage(
+            "Nezapomeňte zkontrolovat své návyky."),
+        "donateToCharity":
+            MessageLookupByLibrary.simpleMessage("Darovat 10\$ na charitu"),
+        "done": MessageLookupByLibrary.simpleMessage("Hotovo"),
+        "editHabit": MessageLookupByLibrary.simpleMessage("Upravit návyk"),
+        "emptyList": MessageLookupByLibrary.simpleMessage("Prázdný seznam"),
+        "exercise": MessageLookupByLibrary.simpleMessage("Cvičení"),
+        "fail": MessageLookupByLibrary.simpleMessage("Nezdařilo se"),
+        "fifteenMinOfVideoGames":
+            MessageLookupByLibrary.simpleMessage("15 min. hraní videoher"),
+        "firstDayOfWeek":
+            MessageLookupByLibrary.simpleMessage("První den v týdnu"),
+        "habit": MessageLookupByLibrary.simpleMessage("Návyk"),
+        "habitContract":
+            MessageLookupByLibrary.simpleMessage("Smlouva o zvyku"),
+        "habitContractDescription": MessageLookupByLibrary.simpleMessage(
+            "Ačkoliv je doporučeno pozitivní posílení, někteří lidé se mohou rozhodnout pro smlouvu o zvyku. Smlouva o zvyku vám umožňuje specifikovat sankci, která bude uložena, pokud svůj návyk zmeškáte, a může zahrnovat partnera, který vám pomáhá sledovat vaše cíle."),
+        "habitDeleted": MessageLookupByLibrary.simpleMessage("Návyk smazán."),
+        "habitLoop": MessageLookupByLibrary.simpleMessage("Smyčka návyků"),
+        "habitLoopDescription": MessageLookupByLibrary.simpleMessage(
+            "Smyčka návyků je psychologický model popisující proces tvorby návyků. Skládá se ze tří komponent: Podnět, Rutina a Odměna. Podnět spustí Rutinu (návykovou akci), která je poté posílena Odměnou, čímž vytváří smyčku, která způsobuje, že je návyk více zakořeněný a pravděpodobnější."),
+        "habitNotifications":
+            MessageLookupByLibrary.simpleMessage("Oznámení o návycích"),
+        "habitNotificationsChannel":
+            MessageLookupByLibrary.simpleMessage("Kanál oznámení pro návyky"),
+        "habitTitleEmptyError": MessageLookupByLibrary.simpleMessage(
+            "Název návyku nesmí být prázdný."),
+        "habits": MessageLookupByLibrary.simpleMessage("Návyky:"),
+        "habo": MessageLookupByLibrary.simpleMessage("Habo"),
+        "haboNeedsPermission": MessageLookupByLibrary.simpleMessage(
+            "Habo potřebuje povolení k odesílání oznámení, aby správně fungoval."),
+        "ifYouWantToSupport": MessageLookupByLibrary.simpleMessage(
+            "Pokud chcete podpořit Habo, můžete:"),
+        "logYourDays":
+            MessageLookupByLibrary.simpleMessage("Zaznamenávejte své dny"),
+        "modify": MessageLookupByLibrary.simpleMessage("Upravit"),
+        "month": MessageLookupByLibrary.simpleMessage("Měsíc"),
+        "noDataAboutHabits": MessageLookupByLibrary.simpleMessage(
+            "Nejsou dostupné žádné informace o návycích."),
+        "notSoSuccessful": MessageLookupByLibrary.simpleMessage("Méně úspěšné"),
+        "note": MessageLookupByLibrary.simpleMessage("Poznámka"),
+        "notificationTime":
+            MessageLookupByLibrary.simpleMessage("Čas oznámení"),
+        "notifications": MessageLookupByLibrary.simpleMessage("Oznámení"),
+        "observeYourProgress":
+            MessageLookupByLibrary.simpleMessage("Sledujte svůj pokrok"),
+        "ohNoSanction":
+            MessageLookupByLibrary.simpleMessage("Ach ne! Vaše sankce:"),
+        "onboarding": MessageLookupByLibrary.simpleMessage("Začínáme"),
+        "privacyPolicy":
+            MessageLookupByLibrary.simpleMessage("Ochrana osobních údajů"),
+        "remainderOfReward": MessageLookupByLibrary.simpleMessage(
+            "Připomínka odměny po úspěšné rutině."),
+        "remainderOfSanction": MessageLookupByLibrary.simpleMessage(
+            "Připomínka sankce po neúspěšné rutině."),
+        "reset": MessageLookupByLibrary.simpleMessage("Resetovat"),
+        "restore": MessageLookupByLibrary.simpleMessage("Obnovit"),
+        "restoreFailedError": MessageLookupByLibrary.simpleMessage(
+            "CHYBA: Obnovení zálohy se nezdařilo."),
+        "reward": MessageLookupByLibrary.simpleMessage("Odměna"),
+        "rewardDescription": MessageLookupByLibrary.simpleMessage(
+            "je výhoda nebo pozitivní pocit, který zažíváte po provedení rutiny. Posiluje návyk."),
+        "rewardNumbered": MessageLookupByLibrary.simpleMessage("3. Odměna"),
+        "routine": MessageLookupByLibrary.simpleMessage("Rutina"),
+        "routineDescription": MessageLookupByLibrary.simpleMessage(
+            "je akce, kterou provádíte jako reakci na podnět. Jedná se o samotný návyk."),
+        "routineNumbered": MessageLookupByLibrary.simpleMessage("2. Rutina"),
+        "sanction": MessageLookupByLibrary.simpleMessage("Sankce"),
+        "save": MessageLookupByLibrary.simpleMessage("Uložit"),
+        "setColors": MessageLookupByLibrary.simpleMessage("Nastavit barvy"),
+        "settings": MessageLookupByLibrary.simpleMessage("Nastavení"),
+        "showMonthName":
+            MessageLookupByLibrary.simpleMessage("Zobrazit název měsíce"),
+        "showReward": MessageLookupByLibrary.simpleMessage("Zobrazit odměnu"),
+        "showSanction": MessageLookupByLibrary.simpleMessage("Zobrazit sankci"),
+        "skip": MessageLookupByLibrary.simpleMessage("Přeskočit"),
+        "skipDoesNotAffectStreaks": MessageLookupByLibrary.simpleMessage(
+            "Přeskočení neovlivňuje série"),
+        "soundEffects": MessageLookupByLibrary.simpleMessage("Zvukové efekty"),
+        "sourceCode":
+            MessageLookupByLibrary.simpleMessage("Zdrojový kód (GitHub)"),
+        "statistics": MessageLookupByLibrary.simpleMessage("Statistiky"),
+        "successful": MessageLookupByLibrary.simpleMessage("Úspěšné"),
+        "termsAndConditions":
+            MessageLookupByLibrary.simpleMessage("Obchodní podmínky"),
+        "theme": MessageLookupByLibrary.simpleMessage("Téma"),
+        "themeSelect": m0,
+        "topStreak": MessageLookupByLibrary.simpleMessage("Nejdelší série"),
+        "total": MessageLookupByLibrary.simpleMessage("Celkem"),
+        "trackYourProgress": MessageLookupByLibrary.simpleMessage(
+            "Můžete sledovat svůj pokrok prostřednictvím kalendářového zobrazení v každém návyku nebo na stránce se statistikami."),
+        "twoDayRule": MessageLookupByLibrary.simpleMessage("Pravidlo dvou dnů"),
+        "twoDayRuleDescription": MessageLookupByLibrary.simpleMessage(
+            "S pravidlem dvou dnů můžete vynechat jeden den a nepřijít o sérii, pokud je další den úspěšný."),
+        "undo": MessageLookupByLibrary.simpleMessage("Vrátit zpět"),
+        "unknown": MessageLookupByLibrary.simpleMessage("Neznámé"),
+        "useTwoDayRule":
+            MessageLookupByLibrary.simpleMessage("Použijte pravidlo dvou dnů"),
+        "warning": MessageLookupByLibrary.simpleMessage("Varování"),
+        "week": MessageLookupByLibrary.simpleMessage("Týden"),
+        "yourCommentHere":
+            MessageLookupByLibrary.simpleMessage("Vaše poznámka zde")
+      };
+}
