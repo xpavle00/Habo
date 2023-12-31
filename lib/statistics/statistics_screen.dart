e96fa8a5f47db082ccf69f80e097950fba6b39c4
Fazil Qazi <96938666+F4fazil@users.noreply.github.com>
2023-12-31 22:50:31 +0500

    Update statistics_screen.dart

--- lib/statistics/statistics_screen.dart
+++ lib/statistics/statistics_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:habo/constants.dart';
-import 'package:habo/habits/habits_manager.dart';
-import 'package:habo/navigation/routes.dart';
-import 'package:habo/statistics/empty_statistics_image.dart';
-import 'package:habo/statistics/overall_statistics_card.dart';
+import 'package:provider/provider.dart';
 import 'package:habo/statistics/statistics.dart';
 import 'package:habo/statistics/statistics_card.dart';
-import 'package:provider/provider.dart';
+import '../constant_helpers/constants.dart';
+import '../habits/habits_manager.dart';
+import '../navigation/routes.dart';
+import 'empty_statistics_image.dart';
+import 'overall_statistics_card.dart';
 
 class StatisticsScreen extends StatefulWidget {
   static MaterialPage page() {
@@ -30,6 +30,7 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text(
           'Statistics',
         ),
@@ -40,31 +41,59 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
           future: Provider.of<HabitsManager>(context).getFutureStatsData(),
           builder:
               (BuildContext context, AsyncSnapshot<AllStatistics> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: HaboColors.primary,
+                ),
+              );
+            } else if (snapshot.hasError || !snapshot.hasData) {
+              return const Center(
+                child: Text('Error loading data'),
+              );
+            } else if (snapshot.data!.habitsData.isEmpty) {
+              return const SizedBox(child: EmptyStatisticsImage());
+            } else {
+              // Your existing code for rendering statistics data
+            }
             if (snapshot.hasData) {
               if (snapshot.data!.habitsData.isEmpty) {
-                return const EmptyStatisticsImage();
+                return const SizedBox(child: EmptyStatisticsImage());
               } else {
-                return ListView(
-                  scrollDirection: Axis.vertical,
-                  physics: const BouncingScrollPhysics(),
-                  children: [
-                    OverallStatisticsCard(
-                      total: snapshot.data!.total,
-                      habits: snapshot.data!.habitsData.length,
-                    ),
-                    ListView(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      children: snapshot.data!.habitsData
-                          .map(
-                            (index) => Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: StatisticsCard(data: index),
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    child: ListView(
+                      scrollDirection: Axis.vertical,
+                      physics: const BouncingScrollPhysics(),
+                      children: [
+                        SizedBox(
+                          child: OverallStatisticsCard(
+                            total: snapshot.data!.total,
+                            habits: snapshot.data!.habitsData.length,
+                          ),
+                        ),
+                        //dulpicate id found error
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            child: ListView(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              children: snapshot.data!.habitsData
+                                  .map((e) => Padding(
+                                        padding: const EdgeInsets.all(10),
+                                        child: StatisticsCard(
+                                          data: e,
+                                        ),
+                                      ))
+                                  .toList(),
                             ),
-                          )
-                          .toList(),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 );
               }
             } else {
