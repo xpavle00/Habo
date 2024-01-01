f15b6aaad049795b99c2ab2d31a369de41ac537f
henok3878 <henokwondimu3878@gmail.com>
2024-01-01 13:28:32 +0300

    Move Habit and Settings models to 'models' folder

---
+++ lib/models/habit_data.dart
@@ -0,0 +1,40 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+class HabitData {
+  HabitData({
+    this.id,
+    required this.position,
+    required this.title,
+    required this.twoDayRule,
+    required this.cue,
+    required this.routine,
+    required this.reward,
+    required this.showReward,
+    required this.advanced,
+    required this.notification,
+    required this.notTime,
+    required this.events,
+    required this.sanction,
+    required this.showSanction,
+    required this.accountant,
+  });
+
+  SplayTreeMap<DateTime, List> events;
+  int streak = 0;
+  int? id;
+  int position;
+  String title;
+  bool twoDayRule;
+  String cue;
+  String routine;
+  String reward;
+  bool showReward;
+  bool advanced;
+  bool notification;
+  TimeOfDay notTime;
+  String sanction;
+  bool showSanction;
+  String accountant;
+}
